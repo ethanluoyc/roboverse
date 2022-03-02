a55a2d2aa716bca6477130ccb0d605615f0c2003
Yicheng Luo <ethanluoyc@gmail.com>
2022-03-02 01:01:22 +0000

    Include package data into the source dist
    find_data_files adapted from dm_control.

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
-from distutils.core import setup
-from setuptools import find_packages
+import fnmatch
+import os
+from setuptools import setup, find_packages
+
+
+def find_data_files(package_dir, patterns, excludes=()):
+    """Recursively finds files whose names match the given shell patterns."""
+    paths = set()
+
+    def is_excluded(s):
+        for exclude in excludes:
+            if fnmatch.fnmatch(s, exclude):
+                return True
+        return False
+
+    for directory, _, filenames in os.walk(package_dir):
+        if is_excluded(directory):
+            continue
+        for pattern in patterns:
+            for filename in fnmatch.filter(filenames, pattern):
+                # NB: paths must be relative to the package directory.
+                relative_dirpath = os.path.relpath(directory, package_dir)
+                full_path = os.path.join(relative_dirpath, filename)
+                if not is_excluded(full_path):
+                    paths.add(full_path)
+    return list(paths)
+
 
 setup(
     name='roboverse',
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'roboverse': find_data_files(
+            'roboverse', ['*.xacro', '*.png', '*.urdf', '*.stl', '*.xml']
+        )
+    },
 )
